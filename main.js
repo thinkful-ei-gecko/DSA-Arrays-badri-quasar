9f6bb4798f9976da70e4d47b187dc8ae04334814
Badri-narayan <badritulsiram@gmail.com>
2019-11-06 13:22:35 -0800

    mergeSort

--- main.js
+++ main.js
@@ -32,6 +32,50 @@ function lessThanFive(numbers) {
   return newNumArray;
 }
 
+// 7. Max sum in the arry
+function maxSum(sequence) {
+  if(!sequence.length){
+    throw new Error('must enter a valid array of nums')
+  }
+  let currentMax = sequence[0]
+  let sum
+  for (let i = 0; i < sequence.length; i++) {
+    sum = sequence[i]
+    for (let j = i + 1; j < sequence.length; j++) {
+      sum += sequence[j]
+      if (sum >= currentMax) {
+        currentMax = sum
+      }
+    }
+  }
+  return currentMax;
+}
+
+// 8. Merge arrays
+function mergeSort(arr1, arr2) {
+  let mergedArr = [];
+  let index1 = 0;
+  let index2 = 0;
+  let current = 0;
+
+  while (current < (arr1.length + arr2.length)) {
+    let isArr1Depleted = index1 >= arr1.length;
+    let isArr2Depleted = index2 >= arr2.length;
+
+    if (!isArr1Depleted && (isArr2Depleted || (arr1[index1] < arr2[index2]))) {
+      mergedArr[current] = arr1[index1];
+      index1++;
+    }
+    else {
+        mergedArr[current] = arr2[index2];
+        index2++;
+      }
+    current++
+    }
+    return mergedArr;
+  }
+  
+
 
 
 function main() {
@@ -46,6 +90,15 @@ function main() {
    console.log(lessThanFive(numArray2));
   */
 
+  // 7.
+  // const sequence = [-3, -5, 15, -4, -6, -10]
+  // console.log(maxSum(sequence))
+
+  // 8.
+  // const arr1 = [1, 3, 6, 8, 11]
+  // const arr2 = [2, 3, 5, 8, 9, 10]
+  // console.log(mergeSort(arr1, arr2))
+  
 }
 
 main();
